470dd963d0d4f207185329d58305a37a0df7ff04
KronSpanca <kronspanca61@gmail.com>
2025-02-27 17:08:02 +0100

    UserInfo model and migration and relationship completed

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use App\Models\UserInfo;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -51,5 +51,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function Userinfo() {
+
+        return $this->hasOne(UserInfo::class, 'user_id', 'id');
     }
 }
